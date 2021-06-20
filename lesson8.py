c174e13928e49783ddfdbc723d763d52ad7ccf7d
surgutneftegas <61916309+surgutneftegas@users.noreply.github.com>
2021-06-20 15:43:45 +0300

    Add files via upload

---
+++ lesson8.py
@@ -0,0 +1,61 @@
+# words = "Hello, \\test 'worlds'"
+# print(words)
+# verse = ('Интегрированная среда разработки для языка программирования Python.\n'
+# 'Предоставляет средства для анализа кода, графический отладчик,')
+# print(verse)
+
+
+#
+# verse = '''\
+# Интегрированная среда разработки для языка программирования Python.
+# средства для анализа кода, графический отладчик,\
+# # '''
+# # print(verse)
+#
+# print(9 > 7 and  2 < 4)
+# print(8 + True)
+# print(True and (True or True) or False)
+#
+# my_input = input("Введите что то")
+# print(bool(my_input))
+
+
+# x = 7
+# if x % 2 == 0:
+#     print(x, "четное число")
+# else:
+#     print(x, "-нечетное")
+
+#
+# heigh = 220
+# age = 18
+# child = 1
+# study_now = 'Yes'
+# if 18 <= age <= 27 and child < 2 and study_now == "No" :
+#     if heigh < 170:
+#         print("В танкисты")
+#     elif heigh < 180:
+#         print("На флот")
+#     elif heigh < 200:
+#         print("В десантники")
+#     else:
+#         print("В другие войска")
+# else:
+#     print("Не призывной")
+
+
+# import this
+# number = 123456
+# if number[0] + number[1] + number[2] == number[3] + number[4] + number[5]:
+#     print("счатсливый")
+#
+# n = int(input("введите"))
+# if n % 2 != 0:
+#     print("Weird")
+# else:
+#     if 2 < n < 5:
+#         print("Not Weird")
+#     if 6 < n < 20:
+#         print("Weird")
+#     if n > 20:
+#         print("Not Weird")
---
+++ lesson9.py
@@ -0,0 +1,12 @@
+# s = r"C:\d\new.txt"
+# print(s)
+# s = "Py" "thon"
+# print(s)
+#
+# s1 = "Hello, "
+# s2 = "world!"
+# s3 = s1 + s2
+# print(s3)
+name = "Anna"
+age = 27
+print("My name is " + name + " and i have " + str(age) + " years"  )
