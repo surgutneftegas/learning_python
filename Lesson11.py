4b08c3fd61bd0cbffd399dea5ef2f193608ec61a
surgutneftegas <61916309+surgutneftegas@users.noreply.github.com>
2021-06-20 15:43:57 +0300

    Add files via upload

---
+++ Lesson11.py
@@ -0,0 +1,23 @@
+# Форматирование строк
+name = 'John'
+age = 30
+
+# print('My name is ' + name + '. I\'m ' + str(age))
+# print('My name is %(name)s. I\'m %(age)d' %{'name': name, 'age': age})
+# print('My name is %s. I\'m %d' %(name,age))
+# print('Title: %s, Price: %.2f' %('Sony', 40))
+# print('My name is %(name)s. I\'m %(age)d' %{'name': name, 'age': age})
+# format
+# print('My name is {}. I\'m {}' .format(name, age))
+# print('My name is {0}. I\'m {1}' .format(name, age))
+# print('My name is {1}. I\'m {0}' .format(name, age))
+# print('My name is {name}. I\'m {age}'.format(name=name, age=age))
+# print('My name is {name}. I\'m {age}'.format(name=age, age=name))
+# print('My name is {x}. I\'m {y}'.format(x=age, y=name))
+
+# f- string
+# print(f'My name is {name}. I\'m  {age}')
+# print(f'My name is {name}. I\'m  {age + 5}')
+# print('5 + 2 = {}'.format(5 + 2))
+# print(f'5 + 2 = {5 + 2}')
+
---
+++ lesson10.py
@@ -0,0 +1,21 @@
+s = 'hello'
+# s = "привет"
+# print(len(s))
+# print(s.capitalize())
+# print(s)
+# s = s.capitalize()
+# # print(s)
+# print(s.center(20, "t"))
+# print(s.count("l"))
+# print(s.count("h"))
+# print(s.count("h", 0, 4))
+#
+# print(s.find("a"))
+# print(s.index("h"))
+# # print(s.replace("l", "s"))
+# s = "hello, world"
+# # print(s.split(","))
+# print(s.isdigit())
+# print(s.isalpha())
+# print(s.isalnum())
+# print(s.islower())
